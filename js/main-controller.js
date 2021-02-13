c597fd3ac3c44ac150d71f675d20a6511a84b665
erez2021 <hershko2013@gmail.com>
2021-02-13 14:27:21 +0200

    some features

--- js/main-controller.js
+++ js/main-controller.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 // importent note:  all this time i was writing on divs,
 // and the buttons and functions are related to it.
@@ -6,119 +6,105 @@
 // I have made some changes but it's still working on divs.
 // hope to fix it till suterday...
 
-
-var elText = document.getElementById('canvas-text')
+var elText = document.getElementById('canvas-text');
 
 function init() {
-    gElCanvas = document.getElementById('canvas')
-    gCtx = gElCanvas.getContext('2d')
-    renderImages()
+	gElCanvas = document.getElementById('canvas');
+	gCtx = gElCanvas.getContext('2d');
+	renderImages();
+}
 
+function download() {
+	var src = gElCanvas.toDataURL('image/png');
+	console.log('src', src);
+
+	var a = document.createElement('a');
+	a.href = src;
+	a.setAttribute('download', 'canvas');
+	document.body.appendChild(a);
+	a.click();
+	document.body.removeChild(a);
 }
 
 function renderImages() {
-    var images = gImgs
-    var strHtml = images.map(function (image) {
-        return `<div class="card"> <img src="/meme-imgs/${image.id}.jpg" onclick="galImgClicked(${image.id})"></div>`
-    })
-    var elImages = document.querySelector('.grid-container')
-    elImages.innerHTML = strHtml.join('')
+	var images = gImgs;
+	var strHtml = images.map(function (image) {
+		return `<div class="card"> <img src="meme-imgs/${image.id}.jpg" onclick="galImgClicked(${image.id})"></div>`;
+	});
+	var elImages = document.querySelector('.grid-container');
+	elImages.innerHTML = strHtml.join('');
 }
 
 function galImgClicked(id) {
-    var hideGal = document.querySelector('.grid-container')
-    hideGal.style.display = 'none'
-    var showMemPage = document.querySelector('.meme-container')
-    showMemPage.style.display = 'block'
-    updateImage(id)
+	var hideGal = document.querySelector('.grid-container');
+	hideGal.style.display = 'none';
+	var showMemPage = document.querySelector('.meme-container');
+	showMemPage.style.display = 'block';
+	var hideSearch = document.querySelector('.search-section')
+	hideSearch.style.display = 'none';
+	updateImage(id);
 }
-
 
 function onTextChange(event) {
-    var txt = event.target.value
-    elText.innerText = event.target.value;
-    elText.style.fontSize = 2.5 + 'rem'
-    elText.style.color = 'white'
-    updateLine(txt, 'txt')
-    console.log(gMeme)
+	var txt = event.target.value;
+	updateLine(txt, 'txt');
+	console.log(gMeme);
 }
 
 
-
-// function draw(ev) {
-//     const {
-//         offsetX,
-//         offsetY
-//     } = ev
-//     drawText('HELLO', offsetX, offsetY)
-// }
-
-
-function onIncreaseText(increaseFactor) {
-    var style = window.getComputedStyle(elText, null).getPropertyValue('font-size');
-    var currentSize = parseFloat(style);
-    elText.style.fontSize = (currentSize + increaseFactor) + 'px';
+function onIncreaseText() {
+	var size = gMeme.lines[gMeme.selectedLineIdx].size;
+	size += 5;
+	if (size === 100) return;
+	updateLine(size, 'size');
 }
 
-
-function onDecreaseText(decreaseFactor) {
-    var style = window.getComputedStyle(elText, null).getPropertyValue('font-size');
-    var currentSize = parseFloat(style);
-    elText.style.fontSize = (currentSize - decreaseFactor) + 'px';
+function onDecreaseText() {
+	var size = gMeme.lines[gMeme.selectedLineIdx].size;
+	size += -5;
+	if (size === 5) return;
+	updateLine(size, 'size');
 }
 
 function onAlignLeft() {
-    var align = gMeme.lines[gMeme.selectedLineIdx].align
-    updateLine(align, 'left')
-    elText.style.marginLeft = 0 + 'rem'
+
+	updateLine('left', 'align');
 }
 
 function onAlignRight() {
-    var align = gMeme.lines[gMeme.selectedLineIdx].align
-    updateLine(align, 'right')
-    elText.style.marginLeft = 18 + 'rem'
+	updateLine('right', 'align');
 }
 
 function onCenterText() {
-    var align = gMeme.lines[gMeme.selectedLineIdx].align
-    updateLine(align, 'center')
-    console.log('align:', align)
-    elText.style.marginLeft = 10 + 'rem'
+	updateLine('center', 'align');
 }
 
-
 function onDeleteText() {
-    var elPlaceholder = document.querySelector('.input')
-    elPlaceholder.value = ''
-    var elDelete = document.querySelector('.canvas-text')
-    elDelete.style.display = 'none'
-
+	var elPlaceholder = document.querySelector('.input');
+	elPlaceholder.value = '';
+	deleteLine();
 }
 
 function onArrowClick() {
-    var elLowerText = document.querySelector('.canvas-text.lower')
-    elText = elLowerText
+	toggleLineIndex();
 }
 
 function onStrokeText() {
-    elText.style.fontWeight = 'bold'
+	toggleFont()
 }
 
 function toggleMenu() {
-    document.body.classList.toggle('open-menu')
+	document.body.classList.toggle('open-menu');
 }
 
 function onChangeColor(ev) {
-    ev.preventDefault()
-    var color = document.querySelector('[name=color]').value
-    elText.style.color = color
-
-    updateLine(color, 'color')
+	ev.preventDefault();
+	var color = document.querySelector('[name=color]').value;
+	updateLine(color, 'color');
 }
 
 function addNewLine() {
-    var elMiddleInput = document.querySelector('.canvas-text.middle')
-    elMiddleInput.style.display = 'block'
-    var elMiddleText = document.querySelector('.canvas-text.middle')
-    elText = elMiddleText
+	createNewLine();
 }
+
+
